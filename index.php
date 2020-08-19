9d2bf341ae4a73e52a378411e756860ab0ec3e29
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2020-08-19 09:09:00 +0100

    Loader fix

--- index.php
+++ index.php
@@ -15,12 +15,13 @@ require_once ".baseinit.php";
     <meta name="publisher" content="<?php echo PRJ_PUBLISHER; ?>">
     <meta name="robots" content='index'>
     <!-- Theming styles -->
-    <link rel="stylesheet" href="/app/soswapp/theme-soswapp/css/theme.min.css">
-    <link rel="stylesheet" href="/app/soswapp/theme-soswapp/css/theme-<?php echo PRJ_THEME; ?>.min.css">
+    <link rel="stylesheet" href="/app/soswapp/font-awesome.soswapp/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/app/soswapp/theme.soswapp/css/theme.min.css">
+    <link rel="stylesheet" href="/app/soswapp/theme.soswapp/css/theme-<?php echo PRJ_THEME; ?>.min.css">
     <!-- optional plugin -->
-    <link rel="stylesheet" href="/app/soswapp/plugin-soswapp/css/plugin.min.css">
-    <link rel="stylesheet" href="/app/soswapp/dnav-soswapp/css/dnav.min.css">
-    <link rel="stylesheet" href="/app/soswapp/faderbox-soswapp/css/faderbox.min.css">
+    <link rel="stylesheet" href="/app/soswapp/plugin.soswapp/css/plugin.min.css">
+    <link rel="stylesheet" href="/app/soswapp/dnav.soswapp/css/dnav.min.css">
+    <link rel="stylesheet" href="/app/soswapp/faderbox.soswapp/css/faderbox.min.css">
     <!-- Project styling -->
     <link rel="stylesheet" href="<?php echo \html_style("base.min.css"); ?>">
   </head>
@@ -44,18 +45,22 @@ require_once ".baseinit.php";
       <div class="view-space">
         <div class="sec-div padding -p20">
           <h1>7 OS Project template</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>
+            <i class="fas fa-user-lock fa-3x"></i>
+          </p>
         </div>
       </div>
     </div>
 
     <!-- Required scripts -->
-    <script src="/app/soswapp/jquery-soswapp/js/jquery.min.js">  </script>
-    <script src="/app/soswapp/js-generic-soswapp/js/js-generic.min.js">  </script>
-    <script src="/app/soswapp/theme-soswapp/js/theme.min.js"></script>
+    <script src="/app/soswapp/jquery.soswapp/js/jquery.min.js">  </script>
+    <script src="/app/soswapp/js-generic.soswapp/js/js-generic.min.js">  </script>
+    <script src="/app/soswapp/theme.soswapp/js/theme.min.js"></script>
     <!-- optional plugins -->
-    <script src="/app/soswapp/plugin-soswapp/js/plugin.min.js"></script>
-    <script src="/app/soswapp/dnav-soswapp/js/dnav.js"></script>
-    <script src="/app/soswapp/faderbox-soswapp/js/faderbox.min.js"></script>
+    <script src="/app/soswapp/plugin.soswapp/js/plugin.min.js"></script>
+    <script src="/app/soswapp/dnav.soswapp/js/dnav.js"></script>
+    <script src="/app/soswapp/faderbox.soswapp/js/faderbox.min.js"></script>
     <!-- project scripts -->
     <script src="<?php echo \html_script ("base.min.js"); ?>"></script>
     <script type="text/javascript">
